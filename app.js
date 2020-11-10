a0e5ef10532ceacaeef878f835ac17598cadb8be
tatialveso <tatiialveso@gmail.com>
2020-11-10 15:25:14 -0300

    create new style

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
-// add new task and send event
-let form = document.querySelector('form');
+const todoInput = document.querySelector('.todoInput');
 
-form.addEventListener('submit', function (event) {
+const todoBtn = document.querySelector('.todoBtn');
+todoBtn.addEventListener("click", addTodo);
+
+function addTodo(event) {
   event.preventDefault();
 
-  let script_card = document.getElementById('placeholder-card')
-  let conteudo_html = script_card.innerHTML
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  todoInput.value = "";
+  newTodo.classList.add("todoItem");
+  todoDiv.appendChild(newTodo);
 
-  let title = document.getElementById('input').value;
+  const completedBtn = document.createElement("button");
+  completedBtn.innerHTML = '<i class="fas fa-check"></i>';
+  completedBtn.classList.add("completedBtn");
+  todoDiv.appendChild(completedBtn);
 
-  conteudo_html = conteudo_html.replace("@title", title);
-  
-  $container = $('#main');
-  $card = $(conteudo_html)
+  const trashBtn = document.createElement("button");
+  trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
+  trashBtn.classList.add("trashBtn");
+  todoDiv.appendChild(trashBtn);
 
-  $container.append($card);
-});
+  todoList.appendChild(todoDiv);
+}
 
-// line-through when task is done
-function taskDone() {
-  let $task = $('#task-title');
-  $task.css({'text-decoration': 'line-through'});
-};
+const todoList = document.querySelector('.todoList');
+todoList.addEventListener("click", deleteCheck);
 
-$('#done').click(taskDone);
+function deleteCheck(e) {
+  const item = e.target;
 
-// delete task
-function cardDelete() {
-  let $delete = $('#card');
+  if(item.classList[0] === 'trashBtn') {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener('transitionend', function() {
+      todo.remove();
+    });
+  }
 
-  $delete.remove();
-};
-
-$('#delete').click(cardDelete);
+  if(item.classList[0] === 'completedBtn') {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
